8315ffb80b5c259400edd16c7ac9031476198823
Justin Lee <leejustin@ucla.edu>
2018-01-01 18:14:58 -0800

    Update content for about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'about',
-    templateUrl: './about.component.html'
+    templateUrl: './about.component.html',
+    styleUrls: ['../blog/post.component.css']
 })
 
 export class AboutComponent {}
