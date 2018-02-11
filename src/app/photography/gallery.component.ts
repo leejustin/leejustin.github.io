3352d4cad1c8a8befbbc08c45d9aa6b5eefabd26
Justin <leejustin@ucla.edu>
2018-02-10 21:48:20 -0800

    Set up photography gallery using nanoGallery

--- src/app/photography/gallery.component.ts
+++ src/app/photography/gallery.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+declare let jquery: any;
+declare let $: any;
 
 @Component({
     selector: 'photography',
-    templateUrl: './gallery.component.html'
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.css']
 })
 
 export class PhotographyComponent {
 
+    private userId: string = '95945723@N06';
+    private album: string = '72157652926219293';
+
+    constructor() { }
+
+    public ngAfterViewInit() {
+
+        $("#nanoGallery").nanogallery2({
+            thumbnailWidth: 'auto',
+            thumbnailHeight: 250,
+            userID: this.userId,
+            kind: 'flickr',
+            photoset: this.album,
+            thumbnailAlignment: "center",
+            thumbnailBorderVertical: 0,
+            thumbnailBorderHorizontal: 0,
+            //galleryDisplayMode: "moreButton",
+            gallerymaxRows: 10,
+            thumbnailLabel: { display: false, position: 'onBottom', align: 'center', displayDescription: 'true' }
+        });
+    }
 }
